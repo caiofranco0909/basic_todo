53882e52ccd0f22033d824d19b12d4a83961359f
Caio Franco <caioporto246@gmail.com>
2024-02-19 00:52:32 -0300

    Adição do restante das funções padrões de controle das samples iniciais

--- src/control/TaskControl.java
+++ src/control/TaskControl.java
@@ -1,5 +1,24 @@
 package control;
 
-public class TaskControl {
+import sample.Task;
 
+public class TaskControl {
+    public Task createTask(String name){
+        increaseCount();
+        return new Task(name);
+    }
+    public void increaseCount(){
+        Task.setCount(Task.getCount()+1);
+    }
+    public void decreaseCount(){
+        Task.setCount(Task.getCount()-1);
+    }
+    public void checkTask(Task task){
+        task.setCheckded(true);
+        decreaseCount();
+    }
+    public void uncheckTask(Task task){
+        task.setCheckded(false);
+        increaseCount();
+    }
 }
--- src/sample/Task.java
+++ src/sample/Task.java
@@ -2,20 +2,37 @@ package sample;
 
 public class Task {
     private String name;
+    private boolean checkded;
     private static int count;
 
     public Task(String name){
         this.name = name;
-        count += 1;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
+
+    public boolean isCheckded() {
+        return checkded;
+    }
+
+    public void setCheckded(boolean checkded) {
+        this.checkded = checkded;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Task.count = count;
+    }
+    
     
 
 }
